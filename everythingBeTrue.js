7b51fde610e3a9cbbb3abb8783baf48f4bd719a6
kemcrimmins <kem.crimmins@gmail.com>
2017-03-30 13:09:54 -0400

    Add files via upload

---
+++ everythingBeTrue.js
@@ -0,0 +1,17 @@
+/* Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+Remember, you can access object properties through either dot notation or [] notation. */
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  
+  var filteredCollection = collection.filter(function(value){
+    return pre in value && Boolean(value[pre]);
+  });
+  
+  if (collection.length === filteredCollection.length) {
+    return true;
+  } else {
+    return false;
+  }  
+}
